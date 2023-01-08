1d7f1d621571dc82c78848cec2d9767703e95ebb
afash7 <afashseven@gmail.com>
2023-01-08 11:34:00 +0330

    first commit - no css style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'PRACTICE1';
-}
+    username: string ='';
+    password: string ='';
+    retypePassword: string = '';
+    isPasswordsMatch: boolean = false;
+
+    public onUsernameInputChanged(e: Event){
+      this.username = (<HTMLInputElement>e.target).value;
+    }
+
+    public onPasswordInputChanged(e: Event){
+      this.password = (<HTMLInputElement>e.target).value;
+    }
+
+    public onRetypePasswordInputChanged(e: Event){
+      this.retypePassword = (<HTMLInputElement>e.target).value
+      // Simple logic code:
+      // if (this.password !== this.retypePassword) {
+      //   this.isPasswordsMatch = false;        
+      // } else {
+      //   this.isPasswordsMatch = true;
+      // }
+      
+      // Profational code is: 
+       this.isPasswordsMatch = this.password !== this.retypePassword ? false : true;
+    }
+  }
+
+ 
